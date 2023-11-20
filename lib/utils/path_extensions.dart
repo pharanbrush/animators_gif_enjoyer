63361440389af4aedcdbed6c625a432ab6b0e8b7
PharanBrush <79798205+pharanbrush@users.noreply.github.com>
2023-11-20 12:51:30 +0800

    Catch more cases for filename handling.

--- lib/utils/path_extensions.dart
+++ lib/utils/path_extensions.dart
@@ -11,15 +11,33 @@ String filenameFromFullPath(String fullPath) {
   return fullPath.split(Platform.pathSeparator).last;
 }
 
-/// Doesn't check the format of the string.
+/// Tries to get the name of the file without the extension.
+///
+/// Excludes everything after the last occurence of a dot.
+/// Returns an empty string if the dot comes at the start of the filename.
+/// Returns the full argument if no dot was found.
+/// Returns an empty string if the argument was empty or whitespace.
 String filenameWithoutExtension(String filenameWithExtension) {
-  final extension = filenameWithExtension.split('.').last;
-  return filenameWithExtension.substring(
-    0,
-    filenameWithExtension.length - extension.length - 1,
-  );
+  if (filenameWithExtension.trim().isEmpty) return '';
+  final dotIndex = filenameWithExtension.lastIndexOf('.');
+  if (dotIndex < 0) return filenameWithExtension;
+  if (dotIndex == 0) return '';
+
+  return filenameWithExtension.substring(0, dotIndex);
 }
 
 String filenameFromFullPathWithoutExtensions(String fullPath) {
   return filenameWithoutExtension(filenameFromFullPath(fullPath));
+}
+
+String? filenameFromUrlWithoutExtension(String url) {
+  final argumentIndex = url.lastIndexOf('?');
+  final lastSlashIndex = url.lastIndexOf('/');
+  final filenameEndIndex =
+      argumentIndex > lastSlashIndex ? argumentIndex : null;
+      
+  var fullFilename = url.substring(lastSlashIndex + 1, filenameEndIndex).trim();
+  if (fullFilename.isEmpty) return null;
+  
+  return filenameWithoutExtension(fullFilename);
 }
